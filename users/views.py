5cb54434f6b7419e2c5363ad2e3c2d79d773368e
Sergey Korolev <bongrichard@rambler.ru>
2024-05-21 21:45:26 +0300

    Выход + регистрация

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
+from django.urls import reverse_lazy
+
+from users.forms import UserRegisterForm
 from django.shortcuts import render
+from django.views.generic import CreateView
+from users.models import User
 
 # Create your views here.
+
+class RegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('users:login')
