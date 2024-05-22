97d061daa47653719df65cbaae4a547a53298967
Sergey Korolev <bongrichard@rambler.ru>
2024-05-22 21:28:03 +0300

    add settings for send_mail and urls for email_confirml

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from blog.functions.email_yandex import password
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -141,3 +141,13 @@ AUTH_USER_MODEL = 'users.User'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = "korolevsergey.veb@yandex.ru"
+EMAIL_HOST_PASSWORD = password
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
+SERVER_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- users/models.py
+++ users/models.py
@@ -8,8 +8,18 @@ class User(AbstractUser):
     email = models.EmailField(unique=True, verbose_name='почта')
 
     phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
-    avatar = models.ImageField(upload_to='users', verbose_name='аватар', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/avatars/', verbose_name='аватар', **NULLABLE)
     country = models.CharField(max_length=50, verbose_name='страна', **NULLABLE)
+
+    token = models.CharField(max_length=150, verbose_name='токен', **NULLABLE)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    def __str__(self):
+        # Строковое отображение объекта
+        return f'{self.email}'
+
+    class Meta:
+        verbose_name = 'Пользователь'  # Настройка для наименования одного объекта
+        verbose_name_plural = 'Пользователи'  # Настройка для наименования набора объектов
